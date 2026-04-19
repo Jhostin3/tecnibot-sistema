1c77f228b80c68bf560365052965a0cd5da174ef
Jhostin3 <jhostinjosue23@gmail.com>
2026-04-19 15:56:33 -0500

    🐛 cancelar suscripciones al desmontar componentes

--- src/features/autenticacion/hooks/ContextoAutenticacion.jsx
+++ src/features/autenticacion/hooks/ContextoAutenticacion.jsx
@@ -66,7 +66,7 @@ export function ProveedorAutenticacion({ children }) {
 
     return () => {
       componenteActivo = false
-      suscripcion.unsubscribe()
+      suscripcion?.unsubscribe()
     }
   }, [cargarPerfil, limpiarSesion])
 
